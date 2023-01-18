b2fa625da2f314ede4cf99628ec05905df7c9023
Ben Chavez <ben.m.chavez@gmail.com>
2023-01-18 11:38:23 -0600

    small change to force reload

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,6 @@
 class Request < ApplicationRecord
   enum status: [:pending, :confirmed, :declined, :cancelled ]
+
   # validates :status, inclusion: {in: [0,1,2,3]}
   belongs_to :user
   belongs_to :event
